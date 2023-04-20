4d55768e1ebbeff8a22d81eb1f6567561d8b7346
Dawid Bryja <dawbryja@gmail.com>
2023-04-20 17:46:35 +0200

    setup for frontend

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
+import livereload from "livereload";
+import connectLivereload from "connect-livereload";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import { dbService } from "./repositories/dbservice.js";
 import { adminRouter } from "./routes/admin/admin.js";
@@ -10,8 +14,21 @@ import { guideSignInRouter } from "./routes/user/signin.js";
 import { guideSignUpRouter } from "./routes/user/signup.js";
 import { guideProfileRouter } from "./routes/user/profile.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const publicDirectory = path.join(__dirname, "public");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(publicDirectory);
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
 const app = express();
-app.use(express.static("public"));
+app.use(connectLivereload());
+app.use(express.static(publicDirectory));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
@@ -27,6 +44,7 @@ app.use(guideProfileRouter);
 app.use(mainRouter);
 app.use(toolsRouter);
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.listen(3000, () => {
